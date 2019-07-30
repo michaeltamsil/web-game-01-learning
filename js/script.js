c622763b61a8240c81e8b720360b0fc09e53cc44
michael tamsil <michaeltamsil@gmail.com>
2019-07-30 18:55:13 +0700

    complete learning phaser

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@
 
 const preload = function () {
     this.load.setBaseURL('http://labs.phaser.io');
-
     this.load.image('sky', 'assets/skies/space3.png');
     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
     this.load.image('red', 'assets/particles/red.png');
@@ -44,8 +43,8 @@ let config = {
         }
     },
     scene: {
-        preload: preload,
-        create: create
+        preload,
+        create
     }
 };
 
